2f4158f51b6c70f889fcd48259de9dc8a4675d42
Pawel <paba.0018@gmail.com>
2022-08-24 17:07:21 +0200

    add new product

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -2,6 +2,19 @@ function adminProductsPage(req, res) {
   res.render("./admin/admin-products");
 }
 
+function getNewProduct(req, res) {
+  res.render("./admin/new-product");
+}
+
+function createNewProduct(req, res) {
+  console.log(req.body);
+  console.log(req.file);
+
+  res.redirect("/admin/products");
+}
+
 module.exports = {
   adminProductsPage: adminProductsPage,
+  getNewProduct: getNewProduct,
+  createNewProduct: createNewProduct,
 };
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin-controllers");
+const imageUploadMiddleware = require("../middlewares/image-upload");
 
 const router = express.Router();
 
 router.get("/", adminController.adminProductsPage);
 
 router.get("/products", adminController.adminProductsPage);
+
+router.get("/new", adminController.getNewProduct);
+
+router.post("/new", imageUploadMiddleware, adminController.createNewProduct);
 
 module.exports = router;
